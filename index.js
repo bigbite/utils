4398fd12053904718272e914ddba103f82320b43
ampersarnie <ampersarnie@gmail.com>
2021-10-05 12:37:33 +0100

    fix: restructure to suit linting and compile with import routes
    Previously had `exports` configured as part of the package to ensure
    that files can be imported via `@bigbite/utils/{type}/{func}` or
    similar. However this could cause numerous false positives with webpack
    and eslinting rules for `import/no-unresolved`. While compiling as
    expected, this rule would strictly compare the route strings of an
    import and the directory structure, so report an error and failure.
    Imports are expected to match the directory structure. To avoid this we
    need to remove the package exports and restructure by bringing files
    down a level from the `src` directory, otherwise imports would have to
    include that full structure; `@bigbite/utils/src/{type}/{func}`

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-export * from "./src";
+import * as Dispatchers from "./dispatchers";
+import * as DOM from "./dom";
+import * as Events from "./events";
+import * as Strings from "./strings";
+
+export default {
+  Dispatchers,
+  DOM,
+  Events,
+  Strings,
+};
