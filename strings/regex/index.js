329a553844f82462c14ce437434e6626306d5461
ampersarnie <ampersarnie@gmail.com>
2021-10-06 09:17:36 +0100

    feat: add function to check that a string is regex pattern

--- strings/regex/index.js
+++ strings/regex/index.js
@@ -1,6 +1,7 @@
 const REGEX_LINEBREAKS = /(\n\s*?\n)\s*\n/;
 
 export default {
+  isRegex: (value) => Object.prototype.toString.call(value) === '[object RegExp]',
   lineBreaks: REGEX_LINEBREAKS,
   replaceLineBreaks: (string) => string.replace(REGEX_LINEBREAKS, '$1'),
 };
